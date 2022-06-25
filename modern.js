e99f93b524b6c4a37234b0204af72c67d743c425
french13 <skswnddl6@gmail.com>
2022-06-25 23:02:35 +0900

    Add files via upload

---
+++ modern.js
@@ -0,0 +1,12 @@
+//변수 데이터 옮기기
+
+let admin;
+let name = "John";
+admin = name;
+//console.log(admin);  => John
+
+let admin;
+let name = "John";
+name = John;  
+//console.log(admin);  => undefined
+
